8515d5eb2826b978ed4f85fa5f00a8bb468ccfc1
Tristochi <TristanLSzakacs@gmail.com>
2019-07-16 14:41:32 -0400

    Final additions before end of shift.

--- FallingBlocks.py
+++ FallingBlocks.py
@@ -8,7 +8,7 @@ BLACK = (0, 0, 0)
 
 
 class Blocks(pygame.sprite.Sprite):
-"""Blocks that will fall from top of screen"""
+	""" Blocks that will fall from top of screen """
 
 	def __init__(self, img):
 		pygame.sprite.Sprite.__init__(self)
@@ -17,19 +17,40 @@ class Blocks(pygame.sprite.Sprite):
 		self.y = y
 		
 	def update(self):
-
+		pass
+		
 	def newPos(self, x, y):
+		pass
+		
 class BlockGame(object):
-"""	Creates block game, controls movement of all blocks. """
+	"""	Creates block game, controls movement of all blocks. """
+	pass
 	
 	
 	
-	
-def main:
+def main():
 	pygame.init()
 	size = [WIDTH, HEIGHT]
 	screen = pygame.display.set_mode(size)
 	
+	#Create instance of blocks. Takes path of image as paramater. 
+	
 	#Create game instance
-	game = BlockGame()
-
+	
+	
+	#Game Loop
+	done = False
+	while not done:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				done = True
+			
+		
+	
+		pygame.display.flip()
+		
+	pygame.quit()
+	
+if __name__ == "__main__":
+	main()
+	
